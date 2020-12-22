90108ce29697f3456cf428bc89a01c5b4faa4ae4
ethan-shenker <eshenker10@stuy.edu>
2020-12-22 17:48:41 -0500

    foundational additions, see devlog

---
+++ app/app.py
@@ -0,0 +1,20 @@
+# Third Huge Freckled Elephant
+# Ethan Shenker, Constance Chen, Andrew Jiang, Saqif Abedin
+# P0 - To Be Named
+# 2020-12-xx
+
+from flask import Flask, render_template, session, request
+import os
+
+app = Flask(__name__)
+app.secret_key = os.urandom(32)
+
+@app.route("/")
+def landing():
+    return render_template("template.html")
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
+    
