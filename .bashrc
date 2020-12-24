0ac901d34d0153bc0c976c6c9cde6e86b3c11388
ethan-shenker <eshenker10@stuy.edu>
2020-12-24 17:18:38 -0500

    added datetime feature to posts

--- .bashrc
+++ .bashrc
@@ -1,1 +1,2 @@
 alias "venv"="source venv/bin/activate"
+alias "go"="venv && cd app && python app.py"
--- app/app.py
+++ app/app.py
@@ -4,8 +4,9 @@
 # 2020-12-xx
 
 from flask import Flask, render_template, session, request
-import sqlite3
 from uuid import uuid4
+import datetime
+import sqlite3
 import os
 import sys
 
@@ -26,39 +27,27 @@ def create_post():
 
 @app.route("/action_create_post")
 def action_create_post():
-
+    
     try:
-        ## user_id will be dependent on the session but users need to be created before
-        ## that can be implemented
-        ## post_id can either be a unique number (uuid module) or some other way we track the variable
-        ## post date--we can import date module to work with that or we can come up with some arbitrary
-        ## tracking system
-
         db = sqlite3.connect("blog.db")
         c = db.cursor()
 
-        '''
-        user_id = 1
-        post_date = "now"
-        '''
-        # user_id = ## user_id
-        # post_date = ## some time method
-
-
+        # user_id = session.get('user_id')
+        # user_id = 1
+        post_id = uuid4() ## random 32-bit ID number
+        post_date = datetime.datetime.now() ## "yyyy-mm-dd hh:mm:ss.xxxxxx"
         post_title = request.args['post_title']
         post_content = request.args['post_content']
-        post_id = uuid4() ## random 32-bit ID number
-
-        query = f"INSERT INTO posts VALUES ({user_id}, '{post_id}', '{post_date}', '{post_title}', '{post_content}');"
-        c.execute(query)
-        
+    
+        # query = f"INSERT INTO posts VALUES ({user_id}, '{post_id}', '{post_date}', '{post_title}', '{post_content}');"
+        # c.execute(query)
         db.commit() 
         db.close()
-        
         return render_template("success.html")
-    
+
     except:
         return render_template("error.html", error=sys.exc_info()[0])
+
 
 if __name__ == '__main__':
     app.debug = True
