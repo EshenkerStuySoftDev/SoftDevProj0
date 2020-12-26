343007cbd52b982cd5b67c6d3409e6a58d304004
ethan-shenker <eshenker10@stuy.edu>
2020-12-26 13:25:13 -0500

    added user functionality

--- app/app.py
+++ app/app.py
@@ -19,6 +19,10 @@ app.secret_key = os.urandom(32)
 def root():
     return landing()
 
+# Function if user tries to access user-only content while not logged in
+def permissions():
+    return render_template("permissions.html")
+
 @app.route("/fake_auth")
 def fake_auth():
     session['user_id'] = int(request.args['user_id'])
@@ -36,49 +40,50 @@ def landing():
         return user_page()
     return render_template("launch.html")
 
-@app.route("/test")
-def test():
-    return render_template("test.html")
-
 ## Route that provides feed -- not pretty at all, simply post entries in raw form
 @app.route("/feed")
 def feed():
-    ## TODO have the feed based on the users that the person logged in is following
-    db = sqlite3.connect("blog.db")
-    c = db.cursor()
-    posts = c.execute("SELECT * FROM posts ORDER BY post_date DESC").fetchall()
-    return render_template("feed.html", posts=posts)
+    if session.get("user_id"):
+        ## TODO have the feed based on the users that the person logged in is following
+        db = sqlite3.connect("blog.db")
+        c = db.cursor()
+        posts = c.execute("SELECT * FROM posts ORDER BY post_date DESC").fetchall()
+        return render_template("feed.html", posts=posts)
+    return permissions()
 
-## Unnecessary (will be taken out), but section found on create_post.html 
+## Unnecessary route (will be taken out), but section found on create_post.html 
 ## should go wherever that functionality ends up
 @app.route("/create_post")
 def create_post():
-    return render_template("create_post.html")
+    if session.get("user_id"):
+        return render_template("create_post.html")
+    return permissions()
 
 @app.route("/action_create_post")
 def action_create_post():
-    db = sqlite3.connect("blog.db")
-    c = db.cursor()
-    user_id = session.get('user_id')
-    post_id = uuid4() ## random 32-bit ID number
-    post_date = str(datetime.datetime.now())[:19] ## "yyyy-mm-dd hh:mm:ss"
-    post_title = request.args['post_title']
-    post_content = request.args['post_content'].strip()
-    query = f"INSERT INTO posts VALUES ({user_id}, '{post_id}', '{post_date}', '{a_clean(post_title)}', '{a_clean(post_content)}')"
-    c.execute(query)
-    db.commit() 
-    db.close()
-    return render_template("launch.html")
-
+    if session.get("user_id"):
+        db = sqlite3.connect("blog.db")
+        c = db.cursor()
+        user_id, post_id, post_date = session.get('user_id'), uuid4(), str(datetime.datetime.now())[:19]
+        post_title, post_content = a_clean(request.args['post_title']), a_clean(request.args['post_content'].strip())
+        query = f"INSERT INTO posts VALUES ({user_id}, '{post_id}', '{post_date}', '{post_title}', '{post_content}')"
+        c.execute(query)
+        db.commit() 
+        db.close()
+        return user_page()
+    return permissions()
+    
 @app.route("/user_page")
 def user_page():
-    user_id = session.get("user_id") ## TODO make home page work only if someone is logged in
-    db = sqlite3.connect("blog.db")
-    c = db.cursor()
-    query = f"SELECT * FROM posts WHERE user_id={user_id}"
-    posts = c.execute(query).fetchall()
-    db.close()
-    return render_template("user_page.html", posts=posts) # user=username)
+    if session.get("user_id"):
+        user_id = session.get("user_id") ## TODO make home page work only if someone is logged in
+        db = sqlite3.connect("blog.db")
+        c = db.cursor()
+        query = f"SELECT * FROM posts WHERE user_id={user_id} ORDER BY post_date DESC"
+        posts = c.execute(query).fetchall()
+        db.close()
+        return render_template("user_page.html", posts=posts) # user=username)
+    return permissions()
 
 
 if __name__ == '__main__':
