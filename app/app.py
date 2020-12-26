ee4317d8993c4c7dca8ef7fbdeb943ed687d756f
ethan-shenker <eshenker10@stuy.edu>
2020-12-26 12:05:10 -0500

    added user functionality

--- app/app.py
+++ app/app.py
@@ -4,9 +4,9 @@
 # 2020-12-xx
 
 from flask import Flask, render_template, session, request
-from uuid import uuid4
-from helpers import a_clean
-import datetime
+from uuid import uuid4      # how to get unique user_id strings
+from helpers import a_clean # function I defined in helpers.py file
+import datetime             # how to get current date / time
 import sqlite3
 import os
 import sys ## we won't need this
@@ -19,27 +19,34 @@ app.secret_key = os.urandom(32)
 def root():
     return landing()
 
+@app.route("/fake_auth")
+def fake_auth():
+    session['user_id'] = int(request.args['user_id'])
+    return root()
+
+@app.route("/logout")
+def logout():
+    if session.get('user_id'):
+        session.pop('user_id')  
+    return root()
+    
 @app.route("/home")
 def landing():
-    if session.get('username'):
-        return render_template("feed.html")
-    ## @ANDREW send me a pm on messenger when u take care of the login functionality bc my next part requires
-    ## sessions to be active
+    if session.get('user_id'):
+        return user_page()
     return render_template("launch.html")
 
 @app.route("/test")
 def test():
     return render_template("test.html")
 
-
-## Route that provides feed -- not pretty at all, simply post objects in raw form
+## Route that provides feed -- not pretty at all, simply post entries in raw form
 @app.route("/feed")
 def feed():
     ## TODO have the feed based on the users that the person logged in is following
     db = sqlite3.connect("blog.db")
     c = db.cursor()
-    c.execute("SELECT * FROM posts ORDER BY post_date DESC")
-    posts = list(c)
+    posts = c.execute("SELECT * FROM posts ORDER BY post_date DESC").fetchall()
     return render_template("feed.html", posts=posts)
 
 ## Unnecessary (will be taken out), but section found on create_post.html 
@@ -52,8 +59,7 @@ def create_post():
 def action_create_post():
     db = sqlite3.connect("blog.db")
     c = db.cursor()
-    # user_id = session.get('user_id') ## TODO once user is logged in, assign post user_id from session profile @andrew
-    user_id = 2
+    user_id = session.get('user_id')
     post_id = uuid4() ## random 32-bit ID number
     post_date = str(datetime.datetime.now())[:19] ## "yyyy-mm-dd hh:mm:ss"
     post_title = request.args['post_title']
@@ -66,15 +72,13 @@ def action_create_post():
 
 @app.route("/user_page")
 def user_page():
-    # username = Session.get("username")
+    user_id = session.get("user_id") ## TODO make home page work only if someone is logged in
     db = sqlite3.connect("blog.db")
     c = db.cursor()
-    user_id = 1
     query = f"SELECT * FROM posts WHERE user_id={user_id}"
-    c.execute(query)
-    posts = list(c)
+    posts = c.execute(query).fetchall()
     db.close()
-    return render_template("user_page.html", posts=posts) # user=username, posts=posts_arr)
+    return render_template("user_page.html", posts=posts) # user=username)
 
 
 if __name__ == '__main__':
