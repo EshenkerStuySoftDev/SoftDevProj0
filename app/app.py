9120429b807323499cf275c5e675c8e7d2f9e7b0
ethan-shenker <eshenker10@stuy.edu>
2020-12-25 23:09:29 -0500

    added homepage mech

--- app/app.py
+++ app/app.py
@@ -14,8 +14,17 @@ import sys ## we won't need this
 app = Flask(__name__)
 app.secret_key = os.urandom(32)
 
+
 @app.route("/")
+def root():
+    return landing()
+
+@app.route("/home")
 def landing():
+    if session.get('username'):
+        return render_template("feed.html")
+    ## @ANDREW send me a pm on messenger when u take care of the login functionality bc my next part requires
+    ## sessions to be active
     return render_template("launch.html")
 
 @app.route("/test")
@@ -26,9 +35,10 @@ def test():
 ## Route that provides feed -- not pretty at all, simply post objects in raw form
 @app.route("/feed")
 def feed():
+    ## TODO have the feed based on the users that the person logged in is following
     db = sqlite3.connect("blog.db")
     c = db.cursor()
-    c.execute("SELECT * FROM posts ORDER BY post_date DESC") ## TODO come up with sorting mechanism
+    c.execute("SELECT * FROM posts ORDER BY post_date DESC")
     posts = list(c)
     return render_template("feed.html", posts=posts)
 
@@ -43,16 +53,28 @@ def action_create_post():
     db = sqlite3.connect("blog.db")
     c = db.cursor()
     # user_id = session.get('user_id') ## TODO once user is logged in, assign post user_id from session profile @andrew
-    user_id = 1
+    user_id = 2
     post_id = uuid4() ## random 32-bit ID number
     post_date = str(datetime.datetime.now())[:19] ## "yyyy-mm-dd hh:mm:ss"
     post_title = request.args['post_title']
     post_content = request.args['post_content'].strip()
-    query = "INSERT INTO posts VALUES (%i, '%s', '%s', '%s', '%s')" % (user_id, post_id, post_date, a_clean(post_title), a_clean(post_content))
+    query = f"INSERT INTO posts VALUES ({user_id}, '{post_id}', '{post_date}', '{a_clean(post_title)}', '{a_clean(post_content)}')"
     c.execute(query)
     db.commit() 
     db.close()
     return render_template("launch.html")
+
+@app.route("/user_page")
+def user_page():
+    # username = Session.get("username")
+    db = sqlite3.connect("blog.db")
+    c = db.cursor()
+    user_id = 1
+    query = f"SELECT * FROM posts WHERE user_id={user_id}"
+    c.execute(query)
+    posts = list(c)
+    db.close()
+    return render_template("user_page.html", posts=posts) # user=username, posts=posts_arr)
 
 
 if __name__ == '__main__':
